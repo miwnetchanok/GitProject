1ac8a22f3e362a0bf3ef3bf4587388e8f133c169
Netchanok <Netchanokmiwnt@hotmail.com>
2021-04-24 16:02:24 +0700

    ข้อสอบครั้งที่4

---
+++ Midterm4/Midterm4/Program.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ข้อ4
+            //float x, y, dx, dy, step;
+            //Console.WriteLine("x1 = ");
+            //float x1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("y1 = ");
+            //float y1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("x2 = ");
+            //float x2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("y2 = ");
+            //float y2 = float.Parse(Console.ReadLine());
+
+            //dx = x2 - x1;
+            //dy = y2 - y1;
+
+            //if (Math.Abs(dx) >= Math.Abs(dx))
+            //{
+            //    step = Math.Abs(dx);
+            //}
+            //else
+            //{
+            //    step = Math.Abs(dy);
+            //}
+            //dx /= step;
+            //dy /= step;
+            //x = x1;
+            //y = y1;
+
+            //for (int i = 0; i < step; i++)
+            //{
+            //    Console.Write(x + "," + y);
+            //    x += dx;
+            //    y += dy;
+            //}
+
+            //ข้อ6
+            //Console.WriteLine("");
+            //string type = Console.ReadLine();
+            //Console.WriteLine("");
+            //int Length = int.Parse(Console.ReadLine());
+            //Console.WriteLine("");
+            //int High = int.Parse(Console.ReadLine());
+            //Console.WriteLine("");
+            //int Time = int.Parse(Console.ReadLine());
+
+            //for (int k = 0; k < Time; k++)
+            //{
+            //    for (int i = 0; i < High; i++)
+            //    {
+            //        {
+            //            Console.WriteLine();
+            //            for (int j = 0; j < Length; j++)
+            //            {
+            //                Console.Write(type);
+            //            }
+
+            //        }
+
+            //    }
+
+            //}
+
+        }
+    }
+}
